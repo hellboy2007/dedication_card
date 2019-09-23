a1051a03164f3951b5d445f10594bf432adbc5b4
lanchunyu <lanchunyu9571@dingtalk.com>
2019-09-23 18:07:45 +0800

    修改

---
+++ js/index.js
@@ -0,0 +1,10 @@
+const tags = document.querySelectorAll('.category .tag');
+const tags_arr = Array.from(tags);
+tags_arr.forEach((item)=>{
+    console.log(item);
+})
+
+
+function create_code() {
+
+}
